ca7d44d1665b520bb9ea35c41282b143931f669e
cristhoperHL <cristhopermma@gmail.com>
2020-03-31 16:56:38 -0500

    added DAta_memory

--- Data_memory.v
+++ Data_memory.v
@@ -1,3 +1,10 @@
-module Data_memory();
+module Data_memory(address,write_data,Memwrite,Memread,read_data);
+
+input [31:0] address,write_data;
+input Memwrite,Memread;
+output [31:0] read_data;
+
+
+
 
 endmodule 
