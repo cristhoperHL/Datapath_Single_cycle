a866bf243a84642bc906757117f3da17825e6923
cristhoperHL <cristhopermma@gmail.com>
2020-04-02 21:41:05 -0500

    aun intentando floating points

--- Float_ALU.v
+++ Float_ALU.v
@@ -10,11 +10,13 @@ output reg [63:0] alu_float_result;
 
 reg sign;
 
-reg [7:0] exponent_s;
+reg [7:0] exponent_s_1;
+
+
 reg [10:0] exponent_d;
 
-reg [22:0] single_precision;
-reg [51:0] double_precision;
+reg [22:0] single_precision_1;
+reg [51:0] double_precision_1;
 
 reg [31:0] data1;//primer operando de tipo single 
 reg [31:0] data2;//segundo operando de tipo single
@@ -29,7 +31,13 @@ begin
         begin
             if(func==6'b000000)
                 begin
+                    sign = read_f_data1[63];
+                    exponent_s_1 = read_f_data1[62:55];
+                    single_precision_1 = read_f_data1[54:32];
+                    exponent_s_1 = exponent_s_1 - 7'b1111111;
+                    //ya esta exponente
 
+                    data1={{32-exponent_s_1{0}},single_precision_1[22:22-exponent_s_1 + 1]};
 
                 end
         end
@@ -37,6 +45,7 @@ begin
         begin
             if(func==6'b000000)
                 begin
+
                     
                 end            
         end
